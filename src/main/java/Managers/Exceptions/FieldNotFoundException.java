b4eece683ca7dc602d28092d70f1ad0c6afc485a
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-11-04 22:42:00 +0530

    Made exceptions for FormManager class to handle form data feeding

--- src/main/java/Managers/Exceptions/FieldNotFoundException.java
+++ src/main/java/Managers/Exceptions/FieldNotFoundException.java
@@ -1,2 +1,13 @@
-package Managers.Exceptions;public class FieldNotFoundException {
+package Managers.Exceptions;
+
+public class FieldNotFoundException extends Exception {
+
+    public FieldNotFoundException() {
+        super("The field, Provided in Keys is not found in WebFields.");
+    }
+
+
+    public FieldNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/java/Managers/Exceptions/NotValidOption.java
+++ src/main/java/Managers/Exceptions/NotValidOption.java
@@ -1,2 +1,13 @@
-package Managers.Exceptions;public class NotValidOption {
+package Managers.Exceptions;
+
+public class NotValidOption extends Exception {
+
+    public NotValidOption() {
+        System.out.println("This is not the valid option for this property!");
+    }
+
+
+    public NotValidOption(String message) {
+        super("This is not the valid option for "+message+" property.");
+    }
 }
